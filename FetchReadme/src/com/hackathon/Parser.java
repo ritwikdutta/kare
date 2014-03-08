a1d03e0268ce88be9bfa0827a8821f497d324c80
Adrian Chmielewski-Anders <a@pyza.net>
2014-03-08 15:55:01 -0800

    working on the tags

--- FetchReadme/src/com/hackathon/Parser.java
+++ FetchReadme/src/com/hackathon/Parser.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
  */
 public class Parser {
 
+    private static ArrayList<String> tags = null;
+
     public static void main(String... args) throws IOException {
         ArrayList<String> keys = getKeywords("JavaScript jQuery BootStrap CDNto");
         for (int i = 0; i < keys.size(); i++) {
@@ -17,34 +19,36 @@ public class Parser {
     }
 
     public static ArrayList<String> getKeywords(String readme) throws IOException {
-
-        System.out.println(new File("MultiPlexer/Data/tags.csv").getAbsolutePath());
-
-        BufferedReader fileReader = new BufferedReader(new FileReader("MultiPlexer/Data/tags.csv"));
-
-        ArrayList<String> tagList = new ArrayList<String>();
-
-
-        while (true) {
-            String str = fileReader.readLine();
-            if (str == null) { break; }
-            tagList.add(str.replaceAll("[\\W]|_", " "));
+        if (tags == null) {
+            init();
         }
-
-
 
         readme = readme.replaceAll("[\\W]|_", " ");
         String[] words = readme.split(" ");
-        ArrayList<String> wordList= new ArrayList<String>();
+        ArrayList<String> wordList= new ArrayList<>();
+
         for (int i = 0; i < 101; i++) {
             wordList.add(words[i]);
         }
-        ArrayList<String> keyWords = new ArrayList<String>();
+        ArrayList<String> keyWords = new ArrayList<>();
         for (String word: wordList) {
-            if (tagList.indexOf(word) != -1 && keyWords.indexOf(word) == -1) {
+            if (tags.indexOf(word) != -1 && keyWords.indexOf(word) == -1) {
                 keyWords.add(word);
             }
         }
         return keyWords;
+    }
+
+    private static void init() throws IOException {
+        System.out.println(new File("MultiPlexer/Data/tags.csv").getAbsolutePath());
+
+        BufferedReader fileReader = new BufferedReader(new FileReader("MultiPlexer/Data/tags.csv"));
+
+        while (true) {
+            String str = fileReader.readLine();
+            if (str == null) { break; }
+            tags.add(str.replaceAll("[\\W]|_", " "));
+        }
+
     }
 }
